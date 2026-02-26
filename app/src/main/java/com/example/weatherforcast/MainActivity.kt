f1ddc87d2712a5bb38f94c15168fcd1de9889bca
mahmoudtarek711 <tmahmoud137@gmail.com>
2026-02-26 21:22:54 +0200

    Testing Env

--- app/src/main/java/com/example/weatherforcast/MainActivity.kt
+++ app/src/main/java/com/example/weatherforcast/MainActivity.kt
@@ -9,7 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.weatherforcast.ui.theme.WeatherForcastTheme
 
@@ -33,7 +38,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = stringResource(R.string.app_name),
         modifier = modifier
     )
 }
